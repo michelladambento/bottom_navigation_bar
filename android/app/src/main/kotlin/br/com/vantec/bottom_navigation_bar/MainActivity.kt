43563a4be4b8835d275a710ca8004abba4573666
michelladambento <michelladamb@gmail.com>
2024-11-27 23:03:38 -0300

    Estudo de bottom navigation bar

---
+++ android/app/src/main/kotlin/br/com/vantec/bottom_navigation_bar/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.vantec.bottom_navigation_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  List<Widget> body = const [
+    Text(
+      "Página Home",
+      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.deepPurple),
+    ),
+    Text(
+      "Página Shoes",
+      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.deepPurple),
+    ),
+    Text(
+      "Página Phones",
+      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.deepPurple),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Navegações",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple[400],
+        elevation: 0,
+      ),
+      body: Center(
+          child: body[_currentIndex]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.grid_view_rounded),
+              label: 'Home'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.store),
+              label: 'Shoes'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_call),
+              label: 'Phones'
+          ),
+        ],
+        onTap: (myIndex) {
+          setState(() {
+            _currentIndex = myIndex;
+          });
+        },
+      ),
+    );
+  }
+}
